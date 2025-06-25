8db054cbf7e33ede5e1b4e20f0eaea78cc3833f7
ferbal <ballarini@gmail.com>
2025-06-25 14:35:31 +0200

    Testing changes during the build

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/portfolio-fernando-ballarini',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/portfolio-fernando-ballarini/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 1753, hash: 'd0ae98a82d9b2bdb5b79d5247b1bbe85130797034fb8016ddd1453390e3a99c5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1594, hash: '09552714afa475237057020a289d15e28dd840080b084f494a4d3fdb856c754c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1782, hash: 'eced44f4bdbfdb88b696b1f9fdf093eb83f46d2aeff5b19a18c0964155af32aa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1623, hash: '6aea18b5916ef0add2370c31eb8d99c7480f248f769dd7f82e1438f34307fac9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-WWWTLGLN.css': {size: 6948, hash: 'kVW+Gfc08Gk', text: () => import('./assets-chunks/styles-WWWTLGLN_css.mjs').then(m => m.default)}
   },
 };
